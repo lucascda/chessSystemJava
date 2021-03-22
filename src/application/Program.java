dec452de3b21c909e393145ac8217038da557acf
Lucas César <lucascesar_35@hotmail.com>
2021-03-22 17:29:57 -0300

    Added class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
-	public Program() {
-		// TODO Auto-generated constructor stub
-	}
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+		
 
 	}
 
